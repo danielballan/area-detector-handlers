b0786da303147b047577457fda926217cdc233e4
Dan Allan <dallan@bnl.gov>
2019-11-14 16:41:41 -0500

    Test closing handlers does not interfere with others.

--- area_detector_handlers/tests/test_hdf5.py
+++ area_detector_handlers/tests/test_hdf5.py
@@ -11,3 +11,21 @@ def test_hdf5(hdf5_files, handler):
             d = h(point_number=frame)
             assert d.shape == expected_shape
             assert np.all(d == frame)
+
+
+@select_handler("AD_HDF5")
+def test_close_one(hdf5_files, handler):
+    "Aim two handlers at the same file. Close one; ensure the other still works."
+    (rpath, kwargs), (N_rows, N_cols, N_points, fpp) = hdf5_files
+    handler1 = handler(rpath, **kwargs)
+    handler2 = handler(rpath, **kwargs)
+    np.asarray(handler1(point_number=0))
+    assert handler1._file is not None
+    np.asarray(handler2(point_number=0))
+    assert handler2._file is not None
+    handler1.close()
+    assert handler1._file is None
+    assert handler2._file is not None
+    assert not handler2._file.closed
+    for frame in range(N_points):
+        np.asarray(handler2(point_number=frame))
